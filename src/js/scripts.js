8bd9ec5fb778d5e347636894c228f02f70b4997c
Mrs. Krabs <47456460+MasterKrab@users.noreply.github.com>
2021-03-05 22:22:19 -0300

    Update

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,15 +1,15 @@
 const checkbox = document.getElementById("duration");
-const pricesElements = document.querySelectorAll(".card__number")
+const pricesElements = document.querySelectorAll(".card__number");
 
 const updatePrices = (duration) =>{
    let num = 0;
 
    for(let type in prices){
-      pricesElements[num].textContent = prices[type][duration]
-      num++
-   }
-}
+      pricesElements[num].textContent = prices[type][duration];
+      num++;
+   };
+};
 
-checkbox.addEventListener("click", () => checkbox.checked ? updatePrices("monthly") : updatePrices("annually"))
+checkbox.addEventListener("click", () => checkbox.checked ? updatePrices("monthly") : updatePrices("annually"));
 
-document.addEventListener("DOMContentLoaded", () => updatePrices("annually"))
+document.addEventListener("DOMContentLoaded", () => updatePrices("monthly"));
